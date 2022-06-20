3a27a70b4fd63036383036fc47448ead1ad25d7c
Alexander Necheff <alex@necheff.net>
2022-06-20 12:38:30 -0400

    Revert "when signing an intermediate CA, symlink from the intermediate/ca/crt to signer/certs/intermediate.crt; fixes issue #8."
    This reverts commit 1ac7030932f27d604709ed3fb603d7a20dac2345.

--- ca.go
+++ ca.go
@@ -279,15 +279,6 @@ func (c *CA) signCSR(csr x509.CertificateRequest, valid int) (certificate Certif
 
 	certificate.certificate = cert
 
-	// Are we signing an intermediate CA? See issue #8.
-	caCsrFile := "/" + certificate.commonName + "/ca/" + certificate.commonName + csrExtension
-	if _, err := storage.LoadFile(caCsrFile); err == nil {
-		// Use a relative path to handle case when $CAPATH is relative, rather than absolute.
-		certFile := "../../" + c.CommonName + "/certs/" + certificate.commonName + "/" + certificate.commonName + certExtension
-		linkFile := "/" + certificate.commonName + "/ca/" + certificate.commonName + certExtension
-		err = storage.LinkFile(certFile, linkFile)
-	}
-
 	return certificate, err
 
 }
